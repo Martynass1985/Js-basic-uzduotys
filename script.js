66a04074eb797ec9b7d05e78aa08e41f2fa3ef0a
martynass1985 <marat19@gmail.com>
2025-01-10 21:32:33 +0200

    sudeta

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ console.log("2. Liekanos paskaičiavimas", daliklis(8, 5))
 
 
 function vidurkis(x, y){
-    let z = (x + y)/2
+    let z = (x + y)/2;
     return "=> Skaičių " + x + " ir " + y + " vidurkis yra " + z.toFixed(2)
 }
 
@@ -23,7 +23,7 @@ console.log ("3. Dviejų skaičių vidurkis", vidurkis(8, 58));
 
 
 function paskaiciuotiSekundes(h, m, s){
-    let x = h*3600 + m*60 + s
+    let x = h*3600 + m*60 + s;
     return "=> Po vidurnakčio prabėgo => " + x + " <= sekundžių."
 }
 
@@ -44,9 +44,9 @@ console.log("5. Ar dalinasi iš 3 ir 5", arDalinasiIs3Ir5(3));
 
 function jonasArPetras(n, m) {
     if (n < m ){
-        return "Petras"
+        return "Petras";
     } 
-    return "Jonas"
+    return "Jonas";
 }
 
 console.log("6. Laimėtojo vardas: ", jonasArPetras(55, 20));
@@ -55,16 +55,16 @@ function kiekKainuosPlyteles(a, b){
     let plotas = a * b;
    let pakuociusk = Math.ceil(((plotas) + (plotas * 0.05)) / 1.5);
    let sumaEur =  pakuociusk * 1.5 * 23;
-   return "Kambario ilgis " + a + "m plotis " + b + "m, plotas " + plotas + "m², reikės " + pakuociusk + " pakuočių plytelių. Reikalinga pinigų suma " + sumaEur + "€"
+   return "Kambario ilgis " + a + "m plotis " + b + "m, plotas " + plotas + "m², reikės " + pakuociusk + " pakuočių plytelių. Reikalinga pinigų suma " + sumaEur + "€";
 }
 
 console.log("7.", kiekKainuosPlyteles(8, 9));
 
 function paverstiMonetomis(n){
    let n1 = (n - (n % 5))/5; 
-   let n2 = (n - n1 * 5 - ((n - n1) % 2))/2
-   let n3 = n - n1 * 5 - n2 * 2
-   let monetuSk = n1 + n2 + n3
-   return "Norint gauti " + n + " sumą litais mums reikės " + n1 + " monetų po 5 Lt, " + n2 + " monetų po 2 lt ir " + n3 + " monetu po 1 Lt. Viso bus " + monetuSk + " monetų"
+   let n2 = (n - n1 * 5 - ((n - n1) % 2))/2;
+   let n3 = n - n1 * 5 - n2 * 2;
+   let monetuSk = n1 + n2 + n3;
+   return "Norint gauti " + n + " sumą litais mums reikės " + n1 + " monetų po 5 Lt, " + n2 + " monetų po 2 lt ir " + n3 + " monetu po 1 Lt. Viso bus " + monetuSk + " monetų";
 }
-console.log("8.", paverstiMonetomis(123))
+console.log("8.", paverstiMonetomis(123));
